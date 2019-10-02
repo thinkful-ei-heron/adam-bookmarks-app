872964b16747486d77a31753fda8632ec2dd084c
Adam Newhouser <adamnewhouser@gmail.com>
2019-10-02 19:26:35 -0400

    attempting webpack config for github

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname + '/',
+    path: __dirname + '/dist',
     filename: 'index_bundle.js'
   },
   mode: 'development',
